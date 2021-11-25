505281c6623a29007378d28770781f52607792ee
Mark Fuller <Mark_Fuller1@baylor.edu>
2021-11-24 20:01:44 -0800

    commit:Wed Nov 24 20:01:44 PST 2021

--- src/main/java/lights/controller/LightsController.java
+++ src/main/java/lights/controller/LightsController.java
@@ -3,6 +3,7 @@ package lights.controller;
 import lights.service.GpioControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +13,14 @@ public class LightsController {
     @Autowired
     GpioControllerService gpioControllerService;
 
-    @RequestMapping("/flash")
+    @RequestMapping("/toggle")
     public String flash() throws InterruptedException {
         return gpioControllerService.toggle();
+    }
+
+    @RequestMapping("/toggle/{times}/{duration}")
+    public String blink(@RequestParam int duration, @RequestParam int times) {
+        return gpioControllerService.blink(times, duration);
     }
 
 }
--- src/main/java/lights/service/GpioControllerService.java
+++ src/main/java/lights/service/GpioControllerService.java
@@ -10,21 +10,30 @@ public class GpioControllerService {
     @Autowired
     DigitalOutput led;
 
-    public String toggle() throws InterruptedException {
-        for (int i = 0; i < 100; i++) {
-            led.high();
-            Thread.sleep(1000);
+    public String toggle() {
+        if (led.isHigh()) {
             led.low();
-            Thread.sleep(1000);
+            return "is now off";
+        } else {
+            led.high();
+            return "is now on";
         }
-        return "done";
     }
-//        if (led.isHigh()) {
-//            led.low();
-//            return "is now off";
-//        } else {
-//            led.high();
-//            return "is now on";
-//        }
-//    }
+
+    public String blink(int times, int duration) {
+        new Thread(() -> {
+            try {
+
+                for (int i = 0; i < times; i++) {
+                    led.high();
+                    Thread.sleep(duration);
+                    led.low();
+                    Thread.sleep(duration);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("Stopped sleep");
+            }
+        }).start();
+        return "blinking";
+    }
 }
