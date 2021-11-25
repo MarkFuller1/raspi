7c741db0801c1ac0c5cf264f35040e56853fd0c4
Mark Fuller <Mark_Fuller1@baylor.edu>
2021-11-25 00:12:02 -0800

    commit:Thu Nov 25 00:12:02 PST 2021

--- src/main/java/lights/service/init/runners/AppStartRunner.java
+++ src/main/java/lights/service/init/runners/AppStartRunner.java
@@ -8,29 +8,29 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Profile("!local")
-@Component
-public class AppStartRunner implements ApplicationRunner {
-
-    @Autowired
-    GpioControllerService gpioControllerService;
-
-    @Autowired
-    TimerService timer;
-
-    @Override
-    public void run(ApplicationArguments args) {
-        gpioControllerService.blink(5, 1000);
-        System.err.println("Initialization complete");
-        new Thread(() -> {
-            while (true) {
-                if (timer.isElapsed()) {
-                    System.out.println("Timer complete");
-                    gpioControllerService.blink(10, 200);
-                    timer.setTimer(0, 0, 0, 0);
-                }
-            }
-        }).start();
-
-    }
-}
+//@Profile("!local")
+//@Component
+//public class AppStartRunner implements ApplicationRunner {
+//
+//    @Autowired
+//    GpioControllerService gpioControllerService;
+//
+//    @Autowired
+//    TimerService timer;
+//
+//    @Override
+//    public void run(ApplicationArguments args) {
+//        gpioControllerService.blink(5, 1000);
+//        System.err.println("Initialization complete");
+//        new Thread(() -> {
+//            while (true) {
+//                if (timer.isElapsed()) {
+//                    System.out.println("Timer complete");
+//                    gpioControllerService.blink(10, 200);
+//                    timer.setTimer(0, 0, 0, 0);
+//                }
+//            }
+//        }).start();
+//
+//    }
+//}
