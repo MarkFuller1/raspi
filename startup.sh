8c0060695814c6771eaee584f63eef7646362866
MarkFuller1 <mark.fuller1999@gmail.com>
2021-11-25 08:58:37 -0800

    add startup

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+sudo touch lastPulled
+sudo echo "$(date)" >> lastPulled
+
+cd /opt/prod/raspi
+
+echo "pulling"
+sudo /usr/bin/mvn clean
+sudo /usr/bin/git pull 
+echo "done pulling"
+echo "compile package"
+sudo /usr/bin/mvn clean compile package 
+echo "done compile package"
+echo "start server"
+sudo /usr/bin/java -jar target/demo-0.0.1-SNAPSHOT.jar 
