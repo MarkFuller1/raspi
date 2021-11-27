e438020564f0f36e7e8ac237504c8cc30f88343e
Mark Fuller <Mark_Fuller1@baylor.edu>
2021-11-27 10:54:33 -0800

    commit:Sat 27 Nov 2021 10:54:33 AM PST

--- src/main/java/lights/service/init/runners/AppStartRunner.java
+++ src/main/java/lights/service/init/runners/AppStartRunner.java
@@ -2,12 +2,23 @@ package lights.service.init.runners;
 
 import lights.service.GpioControllerService;
 import lights.service.TimerService;
+import lights.service.kafka.ProducerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @Profile("!local")
 @Component
 public class AppStartRunner implements ApplicationRunner {
@@ -18,25 +29,35 @@ public class AppStartRunner implements ApplicationRunner {
     @Autowired
     TimerService timer;
 
+    @Autowired
+    ProducerService<String> producerService;
+
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private volatile ScheduledFuture<?> task;
+
     @Override
     public void run(ApplicationArguments args) {
         gpioControllerService.blink(10, 200);
         System.err.println("Initialization complete");
+
         new Thread(() -> {
-            while (true) {
-//                System.out.println("Checking if timer is done...");
+            task = executor.scheduleWithFixedDelay(() -> {
                 if (timer.isElapsed()) {
                     System.out.println("Timer complete");
                     gpioControllerService.blink(10, 200);
                     timer.setTimer(0, 0, 0, 0);
+                    produceTimerMessage();
                 }
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                    System.err.println("Sleep had to end");
-                }
-            }
+            }, 0, 1, TimeUnit.SECONDS);
         }).start();
+    }
 
+    public void produceTimerMessage() {
+        try {
+            producerService.produce("Timer expired:" + Arrays.toString(NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress()));
+        } catch (Exception e) {
+            log.error("Failed to produce the message");
+            log.error(Arrays.toString(e.getStackTrace()));
+        }
     }
 }
