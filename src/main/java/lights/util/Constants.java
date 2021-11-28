3d92a3ef2e2c6997e85c39a199631565cda5e43b
Mark Fuller <Mark_Fuller@Monicas-MacBook-Pro.local>
2021-11-27 21:14:49 -0800

    commit:Sat Nov 27 21:14:49 PST 2021

--- src/main/java/lights/util/Constants.java
+++ src/main/java/lights/util/Constants.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Arrays;
+import java.util.Enumeration;
 
 @Slf4j
 public class Constants {
@@ -12,7 +13,12 @@ public class Constants {
 
     static {
         try {
-            MAC_ADDRESS = Arrays.toString(NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress());
+            for (Enumeration enm = NetworkInterface.getNetworkInterfaces(); enm.hasMoreElements(); ) {
+                NetworkInterface network = (NetworkInterface) enm.nextElement();
+                if (null != network.getHardwareAddress()) {
+                    MAC_ADDRESS = Arrays.toString(network.getHardwareAddress());
+                }
+            }
         } catch (Exception e) {
             log.error("Failed to fetch mac address: " + e.getMessage());
         }
